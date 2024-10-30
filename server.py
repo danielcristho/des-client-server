2eefba04ae79c7f49742270bfc7cd3c9b01fd8ac
rcsinambela <riyanda.c.sinambela@gmail.com>
2024-10-30 22:58:01 +0700

    feat: add log, validate input, and authenticate from client to server

--- server.py
+++ server.py
@@ -1,9 +1,31 @@
 import socket
 import library
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logging.info("Server has started")
+
+
+def validate_input(user_input):
+    if not user_input:
+        logging.warning("Input can't be empty.")
+        return False
+    return True
+
+
+valid_credentials = {"daniel": "tc20", "riyanda": "tc22"}
+
+
+def validate_credentials(username, password):
+    return valid_credentials.get(username) == password
+
 
 def main():
     host = "127.0.0.1"
-    port = 5001
+    port = 5000
     mySocket = socket.socket()
     mySocket.bind((host, port))
 
@@ -13,26 +35,50 @@ def main():
     print("Connection from:", addr)
 
     while True:
+        conn.send("Enter your username: ".encode())
+        username = conn.recv(1024).decode()
+        conn.send("Enter your password: ".encode())
+        password = conn.recv(1024).decode()
+
+        logging.info(f"Username: {username}, Password: {password}")
+
+        if validate_credentials(username, password):
+            logging.info("User authenticated successfully.")
+            conn.send("Authentication successful.".encode())
+            break
+        else:
+            logging.warning("Authentication failed.")
+            conn.send("Authentication failed. Please try again.".encode())
+
+    while True:
         data = conn.recv(1024).decode()
-        print("Received from client =", data)
+        if not data:
+            logging.warning("No data received from client, closing connection.")
+            break
+        print("\nReceived from client =", data)
 
         decryptedMessage = library.decrypt(data)
-        if not data:
-                break
         # Decrypt user's message
         print("Decrypted Message =", decryptedMessage)
         print("\n")
         """
         Send message & encrypt the message
         """
-        message = input("#server -> ")
-        finalEncryptedMessage = library.encrypt(message)
-        print("Encrypted message =", finalEncryptedMessage)
+        while True:
+            message = input("#server -> ")
+            if validate_input(message):
+                logging.info(f"Input received: {message}")
+                finalEncryptedMessage = library.encrypt(message)
+                print("Encrypted message =", finalEncryptedMessage)
 
-        library.sending()
-        conn.send(finalEncryptedMessage.encode())
+                library.sending()
+                conn.send(finalEncryptedMessage.encode())
+                break
+            else:
+                logging.error("Invalid input, please try again.\n")
 
     conn.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
